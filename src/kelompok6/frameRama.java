181716208084b32a240bf429e2d2bd0070b0c616
aliamr <aliamasrukha@gmail.com>
2025-09-13 08:52:25 +0700

    kel6

--- src/kelompok6/frameRama.java
+++ src/kelompok6/frameRama.java
@@ -262,6 +262,8 @@ public class frameRama extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(frameRama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+       
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
